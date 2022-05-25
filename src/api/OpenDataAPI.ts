0bc57cc749ba0b7cc65c94605557c3612c308c76
yuenu <miraiku82@gmail.com>
2022-05-25 19:19:07 +0800

    updated

--- src/api/OpenDataAPI.ts
+++ src/api/OpenDataAPI.ts
@@ -1,13 +1,6 @@
 export const fetcher = async (year: string) => {
   const response = await fetch(
-    `https://www.ris.gov.tw/rs-opendata/api/v1/datastore/ODRP019/${year}`,
-    {
-      method: 'GET',
-      headers: {
-        Connection: 'keep-alive',
-        Host: 'www.ris.gov.tw',
-      },
-    }
+    `https://www.ris.gov.tw/rs-opendata/api/v1/datastore/ODRP019/${year}`
   )
   const res = await response.json()
   return res
